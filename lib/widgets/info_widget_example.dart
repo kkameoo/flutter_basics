5ed70051d37dceae5dd3cf8f4540879171b50625
kkameoo <khdg1202@naver.com>
2025-03-07 15:32:52 +0900

    commit

--- lib/widgets/info_widget_example.dart
+++ lib/widgets/info_widget_example.dart
@@ -5,11 +5,48 @@ class InfoWidgetExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "정보 표시 위젯 예제",
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
+    // return Center(
+    //   child: Text(
+    //     "정보 표시 위젯 예제",
+    //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    //   ),
+    // );
+    return Column(
+      children: <Widget>[
+        Text(
+          'Image Widget: from Assets',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Tooltip(
+          message: "안드로이드 로고입니다",
+          child: Image.asset('assets/images/android.png'),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "Images Widget from Network",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Tooltip(
+          message: "네트워크로부터 불러온 이미지",
+          child: Image.network(
+            "https://petapixel.com/assets/uploads/2016/02/CHL06867.jpg",
+            width: 350,
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "Icon Widgets",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(Icons.favorite, color: Colors.red),
+            Icon(Icons.mail, color: Colors.blue),
+            Icon(Icons.settings, color: Colors.green),
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/widgets/input_widget_example.dart
+++ lib/widgets/input_widget_example.dart
@@ -25,71 +25,106 @@ class _InputWidgetExampleState extends State<InputWidgetExample> {
     //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
     //   ),
     // );
-    return Column(
-      children: <Widget>[
-        TextField(
-          controller: _controller,
-          decoration: const InputDecoration(labelText: "문장을 입력하세요"),
-          onChanged: (text) {
-            setState(() {
-              _inputText = text;
-            });
-          },
-        ),
-        SizedBox(height: 20),
-        Row(
-          children: <Widget>[
-            Checkbox(
-              value: _isChecked,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isChecked = value ?? false;
-                });
-              },
-            ),
-            Text("Check Me!"),
-          ],
-        ),
-        SizedBox(height: 20),
-        Row(
-          children: <Widget>[
-            Radio<int>(
-              value: 0,
-              groupValue: _radioValue,
-              onChanged: (int? value) {
-                setState(() {
-                  _radioValue = value ?? 0;
-                });
-              },
-            ),
-            Text("Option 1"),
-            Radio<int>(
-              value: 1,
-              groupValue: _radioValue,
-              onChanged: (int? value) {
-                setState(() {
-                  _radioValue = value ?? 1;
-                });
-              },
-            ),
-            Text("Option 2"),
-          ],
-        ),
-        SizedBox(height: 20),
-        Slider(
-          value: _sliderValue,
-          min: 0.0,
-          max: 100.0,
-          onChanged: (double value) {
-            setState(() {
-              _sliderValue = value;
-            });
-          },
-        ),
-        Text("Slider Value: ${_sliderValue.toStringAsFixed(1)}"),
-        SizedBox(height: 20),
-        ElevatedButton(onPressed: () {}, child: Text("입력 내용 확인 (AlertDialog)")),
-      ],
+
+    // 입력 방식이 키보드에 가리는 것을 방지하기 위해서 싱글차일드스크롤뷰로 감싼다.
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(labelText: "문장을 입력하세요"),
+            onChanged: (text) {
+              setState(() {
+                _inputText = text;
+              });
+            },
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Checkbox(
+                value: _isChecked,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _isChecked = value ?? false;
+                  });
+                },
+              ),
+              Text("Check Me!"),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Radio<int>(
+                value: 0,
+                groupValue: _radioValue,
+                onChanged: (int? value) {
+                  setState(() {
+                    _radioValue = value ?? 0;
+                  });
+                },
+              ),
+              Text("Option 1"),
+              Radio<int>(
+                value: 1,
+                groupValue: _radioValue,
+                onChanged: (int? value) {
+                  setState(() {
+                    _radioValue = value ?? 1;
+                  });
+                },
+              ),
+              Text("Option 2"),
+            ],
+          ),
+          SizedBox(height: 20),
+          Slider(
+            value: _sliderValue,
+            min: 0.0,
+            max: 100.0,
+            onChanged: (double value) {
+              setState(() {
+                _sliderValue = value;
+              });
+            },
+          ),
+          Text("Slider Value: ${_sliderValue.toStringAsFixed(1)}"),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              //  AlertDialog 띄우기
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("입력 데이터들"),
+                    content: Column(
+                      children: <Widget>[
+                        Text("TextInput: $_inputText"), // 텍스트 입력 상자 값
+                        Text("CheckBox : $_isChecked"),
+                        Text("Radio: $_radioValue"),
+                        Text("Slider: ${_sliderValue.toStringAsFixed(1)}"),
+                      ],
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(); // 스택에서 제거
+                        },
+                        child: const Text('Close'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text("입력 내용 확인 (AlertDialog)"),
+          ),
+          SizedBox(height: 100),
+          TextField(),
+        ],
+      ),
     );
   }
 }
